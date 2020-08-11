83d63da4faf7303008c4eeccd9da1eefa8c2cb7b
Iamrhema <48246378+Iamrhema@users.noreply.github.com>
2020-08-11 10:49:36 +0200

    Update About.js
    Correction "I am"

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,7 +16,7 @@ class About extends Component {
             <h3>Rhema Makamba</h3>
               <div className="about">
                <div style = {{marginTop: "50px"}}> <p >
-                  Hey there I am Rhema a Young innovator with the power of technology in his hands. Im Orignally from the DRC (Democratic Republic of Congo) 
+                  Hey there I'm Rhema a Young innovator with the power of technology in his hands. Im Orignally from the DRC (Democratic Republic of Congo) 
                    I am very passionate about everything tech related because I believe that with technology we can change the world in big ways and small ways.
                   I fell in love with technology at a young age, built my first computer when I was 12 years old, that was when I knew what to do with my future.
                 </p></div>
